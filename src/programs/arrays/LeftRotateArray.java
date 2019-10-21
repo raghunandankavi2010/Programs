6eb0ff0832dab531ab89ac227004882f07e4d4b8
Raghunandan Kavi <raghunandankavi2010@gmail.com>
2019-10-21 23:40:18 +0530

    Array rotation using temp variable.

---
+++ src/programs/arrays/LeftRotateArray.java
@@ -0,0 +1,34 @@
+package programs.arrays;
+
+public class LeftRotateArray {
+
+    public static void main(String[] args){
+        int[] arr ={ 1,2,3,4,5};
+        leftRotateNTimes(arr,2);
+
+    }
+
+    private static void leftRotateNTimes(int[] arr, int n) {
+        int[] rotatedArray = new int[arr.length];
+        for(int i=0;i<=n-1;i++){
+            rotatedArray = leftRotate(arr);
+        }
+        printArray(rotatedArray);
+    }
+
+    private static void printArray(int[] arr) {
+        for(int number : arr){
+            System.out.print(number+" ");
+        }
+    }
+
+    private static int[] leftRotate(int[] arr) {
+        int temp = arr[0];
+        int i;
+        for(i=0;i<arr.length-1;i++){
+            arr[i] = arr[i+1];
+        }
+        arr[i] = temp;
+        return arr;
+    }
+}
