820ad80283271c5125114a9320ac38dee5ea381c
Raghunandan Kavi <raghunandankavi2010@gmail.com>
2019-10-24 22:42:52 +0530

    Re-formatting code with minor changes.

--- src/programs/others/PrimeSum.java
+++ src/programs/others/PrimeSum.java
@@ -1,39 +1,38 @@
 package programs.others;
 
-/*
- * Given an even number ( greater than 2 ), return two prime numbers whose sum will be equal to given number.
- *
- NOTE A solution will always exist. read Goldbach’s conjecture
-
- Example:
-
-
- Input : 4
- Output: 2 + 2 = 4
-
- If there are more than one solutions possible, return the lexicographically smaller solution.
-
- If [a, b] is one solution with a <= b,
- and [c,d] is another solution with c <= d, then
-
- [a, b] < [c, d]
-
- If a < c OR a==c AND b < d.
- */
 import java.util.*;
-public class PrimeSum{
-    public static ArrayList<Integer> primesum(int A) {
-        ArrayList<Integer> result = new ArrayList<Integer>();
-        if(A < 2)
+
+/**
+ * Given an even number ( greater than 2 ), return two prime numbers whose sum will be equal to given number.
+ * NOTE num solution will always exist. read Goldbach’s conjecture
+ *
+ * Example:
+ * Input : 4
+ * Output: 2 + 2 = 4
+ *
+ * If there are more than one solutions possible, return the lexicographically smaller solution.
+ *
+ *  If [a, b] is one solution with a <= b,
+ *  and [c,d] is another solution with c <= d, then
+ *
+ *  [a, b] < [c, d]
+ *
+ *  If a < c OR a==c AND b < d.
+ */
+
+public class PrimeSum {
+    private static ArrayList<Integer> primeSum(int num) {
+        ArrayList<Integer> result = new ArrayList<>();
+        if (num < 2)
             return result;
 
         int first = 2;
-        int second = A - first;
-        while(first <= second){
-            second = A - first;
+        int second = num - first;
+        while (first <= second) {
+            second = num - first;
             System.out.printf("isPrime(%d) : %s \n ", second, isPrime(second));
             System.out.printf("isPrime(%d) : %s \n ", first, isPrime(first));
-            if(isPrime(second) && isPrime(first)){
+            if (isPrime(second) && isPrime(first)) {
                 result.add(first);
                 result.add(second);
                 return result;
@@ -44,22 +43,22 @@ public class PrimeSum{
         return result;
     }
 
-    public static boolean isPrime(int n){
+    private static boolean isPrime(int n) {
 
-        if(n == 2) return true;
-        if(n % 2 == 0)
+        if (n == 2) return true;
+        if (n % 2 == 0)
             return false;
-        int  sqrt = (int) Math.sqrt(n);
-        for(int i = 2; i < sqrt; i++){
-            if(n % i == 0)
+        int sqrt = (int) Math.sqrt(n);
+        for (int i = 2; i < sqrt; i++) {
+            if (n % i == 0)
                 return false;
         }
         return true;
     }
-    public static void main(String[] args){
 
-
-        ArrayList<Integer> result = primesum(16);
+    public static void main(String[] args) {
+        int num = 16;
+        ArrayList<Integer> result = primeSum(num);
         System.out.println(result);
     }
 }
