c08e29e30190ed5eb3aa79d4531e32784dd0e552
Raghunandan Kavi <raghunandankavi2010@gmail.com>
2019-10-25 18:33:00 +0530

    Minor Refactoring.

--- src/programs/strings/StringPermutations.java
+++ src/programs/strings/StringPermutations.java
@@ -4,55 +4,50 @@ package programs.strings;
  * Created by raghu on 3/7/17.
  */
 public class StringPermutations {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
         String input = "abab";
         permutation(input);
-
-    }
-    /** A method exposed to client to calculate permutation of String in Java. */
-
-    private static void permutation(String input) {
-        try {
-            permutation( "", input);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
     }
 
     /**
-     Recursive method which actually prints all permutations
+     * A method exposed to client to calculate permutation of String in Java.
+     */
+
+    private static void permutation(String input) {
+        permutation("", input);
+    }
+
+    /**
+     * Recursive method which actually prints all permutations
      * of given String, but since we are passing an empty String
      * as current permutation to start with,
      * I have made this method private and didn't exposed it to client.
      */
 
-    private static void permutation(String perm, String word) throws InterruptedException {
+    private static void permutation(String perm, String word) {
 
-       //
         if (word.isEmpty()) {
-            //System.out.println(perm);
+
             checkPalindrome(perm);
         } else {
-            for (int i = 0;i < word.length();i++){
-                permutation(perm + word.charAt(i), word.substring(0, i) + word.substring(i + 1,word.length()));
-
+            for (int i = 0; i < word.length(); i++) {
+                permutation(perm + word.charAt(i), word.substring(0, i) + word.substring(i + 1, word.length()));
             }
-
         }
     }
 
     private static void checkPalindrome(String perm) {
 
         String reversed = reverse(perm);
-        if(perm.equals(reversed)){
-            System.out.println("Permuation: "+perm+" is a palindrome");
+        if (perm.equals(reversed)) {
+            System.out.println("Permutation " + perm + " is a palindrome");
         }
     }
 
-    private static String reverse(String input){
+    private static String reverse(String input) {
         StringBuilder builder = new StringBuilder();
-        for(int i=input.length()-1;i>=0;i--){
+        for (int i = input.length() - 1; i >= 0; i--) {
             builder.append(input.charAt(i));
         }
         return builder.toString();
