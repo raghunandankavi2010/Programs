1348c2380d9f795c5db673fea3038981da198de2
Raghunandan Kavi <raghunandankavi2010@gmail.com>
2019-10-25 21:54:45 +0530

    Dynamic programming for fibonacci series.

--- src/programs/hackerank/FibonacciSeries.java
+++ src/programs/hackerank/FibonacciSeries.java
@@ -5,16 +5,38 @@ public class FibonacciSeries {
     public static void main(String[] args){
 
         FibonacciSeries fibonacciSeries = new FibonacciSeries();
-        int value = fibonacciSeries.fib(9);
+        int n = 9;
+        int value = fibonacciSeries.fib(n);
         System.out.println(value);
+        int[] fibs = fibonacciSeries.fib2(n);
+        System.out.println("Fibonacci of nth number:"+fibs[n]);
+        System.out.print("Fibonacci Sequence for 9 numbers : ");
+        for(int number: fibs){
+            System.out.print(number);
+            System.out.print(" ");
+        }
 
     }
 
+    // recursive approach
     private int fib(int n){
         if ( n == 0 )
             return 0;
         else if ( n == 1 )
             return 1;
         return (fib(n-1)+ fib(n-2));
+    }
+
+    // dynamic programming
+    // if you already have calculated fib of any number
+    // re-use it instead of repeating calculation
+    private int[] fib2(int n){
+        int[] fib = new int[n+1];
+        fib[0] = 0;
+        fib[1] = 1;
+        for(int i=2;i<=n;i++){
+            fib[i] = fib[i-1] +fib[i-2];
+        }
+        return fib;
     }
 }
