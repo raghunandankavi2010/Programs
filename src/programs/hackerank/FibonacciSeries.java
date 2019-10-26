a2d84b8172a87c8a23d6392bfc8bdec70aa56c0f
Raghunandan Kavi <raghunandankavi2010@gmail.com>
2019-10-26 21:09:24 +0530

    Base case for fibonacci number.

--- src/programs/hackerank/FibonacciSeries.java
+++ src/programs/hackerank/FibonacciSeries.java
@@ -20,9 +20,7 @@ public class FibonacciSeries {
 
     // recursive approach
     private int fib(int n){
-        if ( n == 0 )
-            return 0;
-        else if ( n == 1 )
+        if ( n <= 2 )
             return 1;
         return (fib(n-1)+ fib(n-2));
     }
