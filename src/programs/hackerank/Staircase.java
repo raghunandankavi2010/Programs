bc176cdcfc4f516802e3b281f6904d4e845e315c
Raghunandan Kavi <raghunandankavi2010@gmail.com>
2019-10-27 21:15:50 +0530

    Hackerrank stair case problem.

---
+++ src/programs/hackerank/Staircase.java
@@ -0,0 +1,40 @@
+package programs.hackerank;
+
+/**
+ * https://www.hackerrank.com/challenges/staircase/problem
+ * 6
+ * Sample Output
+ *
+ *      #
+ *     ##
+ *    ###
+ *   ####
+ *  #####
+ * ######
+ */
+
+public class Staircase {
+
+    // Complete the staircase function below.
+    private static void staircase(int n) {
+        int count = 1;
+        for (int i = 0; i <= n - 1; i++) {
+            for (int j = 0; j <= n - 1 - (count); j++) {
+                System.out.print("0" + " ");
+
+            }
+            for (int k = 0; k < count; k++) {
+                System.out.print("#");
+            }
+            count++;
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 6;
+
+        staircase(n);
+
+    }
+}
