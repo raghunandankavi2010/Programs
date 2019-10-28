50d1dfecab0e1347d5575e93044e84b205a51c7b
Raghunandan Kavi <raghunandankavi2010@gmail.com>
2019-10-28 14:47:43 +0530

    Print the minimum sum path taken.

--- src/programs/matrix/MinumPathSum.java
+++ src/programs/matrix/MinumPathSum.java
@@ -1,6 +1,19 @@
 package programs.matrix;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Input:
+ * [
+ *   [1,3,1],
+ *   [1,5,1],
+ *   [4,2,1]
+ * ]
+ * Output: 7
+ * Explanation: Because the path 1→3→1→1→1 minimizes the sum.
+ */
 public class MinumPathSum {
 
     //1. calculate sum of first row
@@ -10,6 +23,7 @@ public class MinumPathSum {
      private int minCost(int[][] cost,int m,int n){
 
         int[][] dp = new int[m+1][n+1];
+         List<Integer> mList = new ArrayList<>();
         int sum = 0;
         for(int i=0; i <= n; i++){
             dp[0][i] = sum + cost[0][i];
@@ -27,6 +41,33 @@ public class MinumPathSum {
                 dp[i][j] = cost[i][j] + Math.min(dp[i-1][j], dp[i][j-1]);
             }
         }
+         System.out.println("Dp array sum");
+         for(int i=0; i <= m; i++){
+             for(int j=0; j <= n; j++){
+              System.out.print(dp[i][j]+" ");
+             }
+             System.out.println(" ");
+         }
+
+
+         mList.add(cost[m][n]);
+         for(int i=m; i >=0 ; i--){
+             for(int j=n; j >=0 ; j--){
+                 if(i==0 || j==0){
+                    break;
+                 }
+                 if(dp[j][j-1]<dp[j-1][j]){
+                     mList.add(cost[j][j-1]);
+                 }else{
+                     mList.add(cost[i-1][i]);
+                 }
+             }
+         }
+         System.out.println("Path taken for minimum sum");
+         for(int j= mList.size()-1;j>=0;j--){
+             System.out.print(mList.get(j)+" ");
+         }
+         System.out.println("");
         return dp[m][n];
     }
 
@@ -42,8 +83,6 @@ public class MinumPathSum {
         int row = 2;
         int column = 2;
         int result = mcp.minCost(cost, row, column);
-        System.out.println(result);
+        System.out.println("Minimum cost: "+result);
     }
-
-
 }
