af04894e4fc7415cb6a6d27beaf12248d9400ca7
Raghunandan Kavi <raghunandankavi2010@gmail.com>
2019-10-28 12:12:21 +0530

    pyramid print.

--- src/programs/hackerank/Staircase.java
+++ src/programs/hackerank/Staircase.java
@@ -20,7 +20,7 @@ public class Staircase {
         int count = 1;
         for (int i = 0; i <= n - 1; i++) {
             for (int j = 0; j <= n - 1 - (count); j++) {
-                System.out.print("0");
+                System.out.print(" ");
             }
             for (int k = 0; k < count; k++) {
                 System.out.print("#");
@@ -32,8 +32,24 @@ public class Staircase {
 
     public static void main(String[] args) {
         int n = 6;
-
         staircase(n);
+        System.out.println("Pyramid");
+        printPyramid(n);
+    }
 
+    private static void printPyramid(int n){
+        int i, space, rows, k=0;
+        rows = n;
+        for(i=1; i<=rows; i++) {
+            for(space=1; space<=(rows-i); space++) {
+                System.out.print("  ");
+            }
+            while(k != (2*i-1)) {
+                System.out.print("# ");
+                k++;
+            }
+            k = 0;
+            System.out.println();
+        }
     }
 }
