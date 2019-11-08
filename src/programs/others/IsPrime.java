ef4d11574ccd13f20b2d6378f2337f6da0f85de5
Raghunandan Kavi <raghunandankavi2010@gmail.com>
2019-11-08 15:11:46 +0530

    Add Comments

--- src/programs/others/IsPrime.java
+++ src/programs/others/IsPrime.java
@@ -7,6 +7,13 @@ public class IsPrime {
         System.out.println(" Number is prime or not "+isPrime(num));
     }
 
+    /**
+     *
+     * @param num - a positive integer
+     * @return true if its prime else false
+     * if you need to check 13 is prime or not
+     * you need to check till 6 because 6*2 = 12.
+     */
     private static boolean isPrime(int num){
         for(int i=2;i<=(num/2);i++) {
             if (num % i == 0) {
