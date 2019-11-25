ddf09314944cbe8c4681286f19b66bf862912bec
Raghunandan Kavi <raghunandankavi2010@gmail.com>
2019-11-25 15:55:55 +0530

    maximum profit

---
+++ src/programs/arrays/Profit.java
@@ -0,0 +1,24 @@
+package programs.arrays;
+
+/**
+ * https://leetcode.com/problems/best-time-to-buy-and-sell-stock/submissions/
+ */
+public class Profit {
+
+    public static void main(String[] args) {
+        int[] arr= {7,1,5,3,6,4};
+        calculateProfit(arr);
+    }
+
+    private static int calculateProfit(int[] arr) {
+        if(arr.length == 0) return 0;
+        int maxP = 0, minS = arr[0];
+        for(int p:arr){
+            minS = Math.min(minS,p);
+
+            maxP = Math.max(maxP,p-minS);
+        }
+        return maxP;
+
+    }
+}
