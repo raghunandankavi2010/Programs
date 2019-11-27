3d559724dcc83ee62404b68cd11d455a8e9fc642
Raghunandan Kavi <raghunandankavi2010@gmail.com>
2019-11-27 22:17:17 +0530

    kth Largest element in array using heap.

--- src/programs/arrays/KthSmallestElementInArray.java
+++ src/programs/arrays/KthSmallestElementInArray.java
@@ -8,9 +8,11 @@ public class KthSmallestElementInArray {
 
     public static void main(String[] args) {
         int[] arr= {2,3,15,1,0,23,21};
-        int k=3;
+        int k=2;
         int kthSmallestElement = findKthSmallestElement(arr,k);
+        int kthLargestElement = findKthLargestElement(arr,k);
         System.out.println(String.format("Smallest %d element is %d",k,kthSmallestElement));
+        System.out.println(String.format("Largest %d element is %d",k,kthLargestElement));
     }
 
     private static int findKthSmallestElement(int[] arr, int k) {
@@ -26,10 +28,35 @@ public class KthSmallestElementInArray {
         // 2 1 0
         for(int i=k;i<arr.length;i++){
             if(arr[i]<pq.peek()){
-                pq.poll(); // remove fr
+                pq.poll(); // remove from queue
                 pq.add(arr[i]);
             }
         }
         return pq.peek(); // return 2
+    }
+
+    private static int findKthLargestElement(int[] arr, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int i=0;i<k;i++){
+            pq.add(arr[i]); //2 3 15
+        }
+        // from 1 to 21
+        // arr[i] now is 1
+        // 1 > 2 skip
+        // 0 > 2 skip
+        // 3 > 23 true
+        // remove 2 and add 23
+        // 3 15 23
+        // 21 > 3
+        // remove 3 and add 21
+        // 15 23 21
+        // return 15
+        for(int i=k;i<arr.length;i++){
+            if(arr[i]>pq.peek()){
+                pq.poll();
+                pq.add(arr[i]);
+            }
+        }
+        return pq.peek();
     }
 }
