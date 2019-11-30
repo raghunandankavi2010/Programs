bd3fb6d80709b2680018c2f49f2626f2b836aae8
Raghunandan Kavi <raghunandankavi2010@gmail.com>
2019-11-30 18:01:24 +0530

    Print matrix in spiral form. Handle edge cases.

--- src/programs/matrix/SpiralTraversalMatrix.java
+++ src/programs/matrix/SpiralTraversalMatrix.java
@@ -6,9 +6,9 @@ public class SpiralTraversalMatrix {
         int[][] matrix = {{1, 2 ,3 ,4},
                 {5 ,6 ,7 ,8 },
                 {9, 10, 11, 12},
-                {13 ,14 ,15 ,16}};
+                {13,14,15,16}};
         
-        int m = 4,n=4;
+        int m = matrix.length,n=matrix[0].length;
         matrixSpiralTraversal(matrix,m,n);
     }
 
@@ -19,19 +19,19 @@ public class SpiralTraversalMatrix {
          while(start_row<m && start_column<n){
 
              // print first row
-             for(i=start_row;i<n;i++){
-                 System.out.print(matrix[start_row][i]+" ");
+             for(i=start_column;i<n;i++){
+                 System.out.print(matrix[start_column][i]+" ");
              }
              start_row++; //printed first row increase start_row index
              // print first column
              for(i=start_row;i<m;i++){
-                 System.out.print(matrix[i][m-1]+" ");
+                 System.out.print(matrix[i][n-1]+" ");
              }
              n--; // decrease end column index
 
              // print last row in reverse
              if (start_row < m) {
-                 for (i = n-1; i >= 0; i--) {
+                 for (i = n-1; i >= start_column; i--) {
                      System.out.print(matrix[m-1][i] + " ");
                  }
                  m--; // decrease row  end index
@@ -39,7 +39,7 @@ public class SpiralTraversalMatrix {
 
              // print first column
              if (start_column < n) {
-                 for (i = n-1; i >= start_row; i--) {
+                 for (i = m-1; i >= start_row; i--) {
                      System.out.print(matrix[i][start_column] + " ");
                  }
                  start_column++; // increase start column index;
