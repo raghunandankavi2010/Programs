18d2eefba9a475a817c2dec9e27b4f173cd2e527
Raghunandan Kavi <raghunandankavi2010@gmail.com>
2019-12-20 15:32:09 +0530

    Maximum Sub Array sum variation problem.

---
+++ src/programs/arrays/MaximumSubArray.java
@@ -0,0 +1,58 @@
+package programs.arrays;
+
+import org.javatuples.Pair;
+
+/**
+ * Find out the maximum sub-array of non negative numbers from an array.
+ * A : [1, 2, 5, -7, 2, 3]
+ * The two sub-arrays are [1, 2, 5] [2, 3].
+ * The answer is [1, 2, 5] as its sum is larger than [2, 3]
+ */
+
+public class MaximumSubArray {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 5, -7,-7,8,9};
+        Pair<Integer,Integer> pair = findSubArray(arr);
+        System.out.println("Positive Sub Array whose sum is greater");
+        for(int i=pair.getValue0();i<pair.getValue1();i++){
+            System.out.print(arr[i]+" ");
+        }
+
+    }
+
+    // calculate sum of elements till you find the negative number
+    // startIndex is set to 0 initially
+    // endIndex after calculating sum is i
+    // check if sum calculated is greater than previous sum calculated
+    // if so startIndex is pointing of the sub array sum calculation
+    // endIndex is again pointing to i
+    private static Pair<Integer,Integer> findSubArray(int arr[]) {
+        int n = arr.length;
+        int startIndex = 0;
+        int endIndex = 0;
+        int sum = 0;
+        int newSum = 0;
+        int beginIndex;
+
+        for (int i = 0; i < n; i++) {
+            beginIndex = i;
+            while (i < n && arr[i] >= 0) {
+                sum = sum + arr[i];
+                i++;
+            }
+
+            if (sum > newSum) {
+                startIndex = beginIndex;
+                endIndex = i;
+                newSum = sum;
+            }
+            sum = 0;
+        }
+
+        System.out.println("Sum of sub array that is greater: "+newSum);
+        return new Pair<>(startIndex,endIndex);
+    }
+}
+
+
