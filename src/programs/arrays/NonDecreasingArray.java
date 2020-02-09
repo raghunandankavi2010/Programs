ac98bd4082694ae59bac5fa1633c11416434dd7b
Raghunandan Kavi <raghunandankavi2010@gmail.com>
2020-02-09 11:24:31 +0530

    Non decreasing array simple easy to understand solution.

--- src/programs/arrays/NonDecreasingArray.java
+++ src/programs/arrays/NonDecreasingArray.java
@@ -5,27 +5,25 @@ public class NonDecreasingArray {
 
     public static void main(String[] args) {
         NonDecreasingArray nonDecreasingArray = new NonDecreasingArray();
-        int[] arr = {4,2,1};
+        int[] arr = {4,2};
         System.out.println(nonDecreasingArray.checkPossibility(arr));
     }
 
     private boolean checkPossibility(int[] nums) {
         int count = 0;
-        int index =-1;
-        for ( int i = 0; i< nums.length-1;i++){
-            if(nums[i] > nums[i+1]){
-                index = i;
-                count++;
+        int length = nums.length-1;
+        for (int i = 0; i < length - 1 && count <= 1; ++i) {
+            if (nums[i] > nums[i + 1]) { // e.g. [2,2,3,2,4] 3>2
+                if (i > 0) {
+                    if (nums[i - 1] <= nums[i + 1]) // if number is lesser or equal modify nums[i]
+                        nums[i] = nums[i - 1];   // to the previous number. 2<=2 modify nums[i] =2
+                    else                          // in all other cases modify the next number
+                        nums[i + 1] = nums[i]; // to the current number to match// the sequence
+                }
+                ++count;
             }
         }
-        if(count > 1)
-            return false;
-        if(count ==1){
-            if(index+1 == nums.length-1 || index == 0)
-                return true;
 
-            return  (nums[index]<nums[index+2] || nums[index-1]<nums[index+1]);
-        }
-        return  true;
+        return  count<=1;
     }
 }
