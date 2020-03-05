88a729c286bd3d48beb565616dcc0f3012dd52b6
Raghunandan Kavi <raghunandankavi2010@gmail.com>
2020-03-05 10:39:50 +0530

    Minor refactoring.

--- src/programs/arrays/NonDecreasingArrayVariation.java
+++ src/programs/arrays/NonDecreasingArrayVariation.java
@@ -12,34 +12,30 @@ public class NonDecreasingArrayVariation {
     // Complete the bigSorting function below.
     static String[] bigSorting(String[] unsorted) {
 
-        Arrays.sort(unsorted, new Comparator<String>() {
-            @Override
-            public int compare(String x, String y) {
-                // If the length is not the same, we return the difference.
-                // A negative # means, x Length is shorter, 0 means the same (this doesn't occur) and a postive # means Y is bigger
-                if (x.length() != y.length()) return x.length() - y.length();
+        Arrays.sort(unsorted, (x, y) -> {
+            // If the length is not the same, we return the difference.
+            // A negative # means, x Length is shorter, 0 means the same (this doesn't occur) and a postive # means Y is bigger
+            if (x.length() != y.length()) return x.length() - y.length();
 
-                // Now the length is the same.
-                // Compare the number from the first digit.
-                char[] xarray = x.toCharArray();
-                char[] yarray = y.toCharArray();
-                for (int i = 0; i < x.length(); i++) {
-                    char left = xarray[i];
-                    char right = yarray[i];
-                    if (left != right)
-                        return left - right;
-                }
-
-                // Default: "0" means both numbers are the same.
-                return 0;
+            // Now the length is the same.
+            // Compare the number from the first digit.
+            char[] xarray = x.toCharArray();
+            char[] yarray = y.toCharArray();
+            for (int i = 0; i < x.length(); i++) {
+                char left = xarray[i];
+                char right = yarray[i];
+                if (left != right)
+                    return left - right;
             }
+
+            // Default: "0" means both numbers are the same.
+            return 0;
         });
         return unsorted;
 
     }
 
     public static void main(String[] args)  {
-       int size = 6;
        String[] arr = {"31415926535897932384626433832795",
                "1",
                "3",
