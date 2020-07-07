400d94fcda8d469df2205d25cba53ba1e42ea8e2
Raghunandan Kavi <raghunandankavi2010@gmail.com>
2020-07-07 13:21:22 +0530

    Minor change

--- src/programs/threads/BillPloughSingleton.java
+++ src/programs/threads/BillPloughSingleton.java
@@ -1,16 +1,22 @@
 package programs.threads;
 
 
-public class BillPloughSingleton {
+ class Singleton {
 
     // inner static class ensures one instance always
     // when class is loaded inner class is not loaded
    private static class StaticHelper {
-        private static final BillPloughSingleton billPloughSIngleton = new BillPloughSingleton();
+        private static final Singleton billPloughSingleton = new Singleton();
     }
 
     // instance created whe first time call to getInstance
-    private static BillPloughSingleton getInstance(){
-        return StaticHelper.billPloughSIngleton;
+    public static Singleton getInstance(){
+        return StaticHelper.billPloughSingleton;
+    }
+}
+
+public class BillPloughSingleton{
+    public static void main(String[] args) {
+       Singleton singleton =  Singleton.getInstance();
     }
 }
