f429aa97482740c95b315becb9e56bd40e884c7c
Raghunandan Kavi <raghunandan2005@gmail.com>
2021-01-14 15:27:45 +0530

    Convert java to kotlin.

--- src/programs/search/BinarySearch.kt
+++ src/programs/search/BinarySearch.kt
@@ -1,41 +1,42 @@
-package programs.search;
+package programs.search
+
+import kotlin.jvm.JvmStatic
+import programs.search.BinarySearch
 
 /**
- *  Search in sorted array binary search
- *  o(logn)
+ * Search in sorted array binary search
+ * o(logn)
  */
-
-public class BinarySearch {
-
-    public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        int key = 2;
-        BinarySearch binarySearch = new BinarySearch();
-        int low = 0;
-        int high = arr.length - 1;
-        int searchIndex = binarySearch.binarySearch(arr, key, low, high);
-        if (searchIndex != -1)
-            System.out.println("Number :" + arr[searchIndex] +" found at index "+ searchIndex);
-        else
-            System.out.println("Element not found");
+class BinarySearch {
+    private fun binarySearch(arr: IntArray, key: Int, low: Int, high: Int): Int {
+        if (low > high) return -1
+        if (low == high && arr[low] == key) return low
+        val mid = (low + high) / 2
+        if (arr[mid] == key) return mid
+        return when {
+            arr[mid] < key -> binarySearch(arr, key, mid + 1, high)
+            arr[mid] > key -> binarySearch(
+                arr,
+                key,
+                low,
+                mid - 1
+            )
+            else -> -1
+        }
     }
 
-    private int binarySearch(int[] arr, int key, int low, int high) {
-
-        if (low > high)
-            return -1;
-        if (low == high && arr[low] == key)
-            return low;
-        int mid = (low + high) / 2;
-
-        if (arr[mid] == key)
-            return mid;
-        if (arr[mid] < key)
-            return binarySearch(arr, key, mid + 1, high);
-        else if (arr[mid] > key)
-            return binarySearch(arr, key, low, mid-1);
-        else
-            return -1;
-
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val arr = intArrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+            val key = 2
+            val binarySearch = BinarySearch()
+            val low = 0
+            val high = arr.size - 1
+            val searchIndex = binarySearch.binarySearch(arr, key, low, high)
+            if (searchIndex != -1) println("Number :" + arr[searchIndex] + " found at index " + searchIndex) else println(
+                "Element not found"
+            )
+        }
     }
 }
--- src/programs/search/MissingElementSearch.kt
+++ src/programs/search/MissingElementSearch.kt
@@ -1,31 +1,32 @@
-package programs.search;
+package programs.search
 
-public class MissingElementSearch {
+import kotlin.jvm.JvmStatic
+import programs.search.MissingElementSearch
 
-    public static void main(String[] args) {
-        int[] arr = {1,3,4,5};
-        int x = findMissingElement(arr);
-        System.out.println(x);
+object MissingElementSearch {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val arr = intArrayOf(1, 3, 4, 5)
+        val x = findMissingElement(arr)
+        println(x)
     }
 
-    static int findMissingElement(int[] arr){
-        int low = 0;
-        int high = arr.length-1;
-
-        while(low<=high){
-            int middle = (low+high)/2;
+    private fun findMissingElement(arr: IntArray): Int {
+        var low = 0
+        var high = arr.size - 1
+        while (low <= high) {
+            val middle = (low + high) / 2
             // this is the most important condition which i missed
-            if(arr[middle]!= middle+1 && arr[middle-1]==middle){
-                return middle+1;
+            if (arr[middle] != middle + 1 && arr[middle - 1] == middle) {
+                return middle + 1
             }
             // this is straight forward. if arr[mid] != middle+1 move left or move right
-            if(arr[middle]!= middle+1){
-                high = middle-1;
+            if (arr[middle] != middle + 1) {
+                high = middle - 1
             } else {
-                low = middle+1;
-
+                low = middle + 1
             }
         }
-        return -1;
+        return -1
     }
 }
