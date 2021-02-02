9ac2ae717f6bcdc022dc680ba6d254548d7155e2
Raghunandan Kavi <raghunandan2005@gmail.com>
2021-02-02 18:32:01 +0530

    Find pair whose sum is equal to l

---
+++ src/programs/arrays/FindPair.kt
@@ -0,0 +1,24 @@
+package programs.arrays
+
+import java.util.HashMap
+
+/**
+ *  Find pairs whose sum is K given an array of integers
+ *  Do it in one pass
+ *  One way is to sort array and then increment low and high based on sum greater os lesser than k
+ */
+fun main() {
+    val intArray = intArrayOf(10,25,5,7,20,3)
+    val k = 35
+
+    val complementMap: HashMap<Int, Int> = HashMap()
+
+    for (i in intArray.indices) {
+        val curr: Int = intArray[i]
+        if (complementMap.containsKey(k - curr)) {
+          println("Pair is $curr ${k-curr}")
+        }
+        complementMap[curr] = i
+    }
+
+}
