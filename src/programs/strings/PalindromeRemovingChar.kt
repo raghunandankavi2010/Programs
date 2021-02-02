258c8470c9e1cb85995996f6fdd93eba7ac0634a
Raghunandan Kavi <raghunandan2005@gmail.com>
2021-02-02 17:43:55 +0530

    Converting to kotlin and checking if its palindrome using set.

--- src/programs/strings/PalindromeRemovingChar.kt
+++ src/programs/strings/PalindromeRemovingChar.kt
@@ -1,44 +1,58 @@
-package programs.strings;
+package programs.strings
 
-public class PalindromeRemovingChar {
-
-    public static void main(String[] args) {
-        String str = "aaa";
-        int index = possiblePalinByRemovingOneChar(str);
-        System.out.println(index);
+object PalindromeRemovingChar {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val str = "Mad"
+        val isPalindrome = isPalin(str)
+        println("$str is Palindrome? $isPalindrome")
+        val index = possiblePalinByRemovingOneChar(str)
+        println(index)
     }
 
-    private static int possiblePalinByRemovingOneChar(String str) {
-            int low = 0, high = str.length()-1;
-            while(low<high){
-                if(str.charAt(low)==str.charAt(high)){
-                    low++;
-                    high--;
-                }else{
-                    // check if removing the left char forms a palindrome
-                    if (isPalindrome((low+1), high,str))
-                        return low;
-
-                    // check if removing the right char forms a palindrome
-                    if (isPalindrome(low,high-1,str))
-                        return high;
-
-                }
-            }
-            return -1;
-    }
-
-    private static boolean isPalindrome(int low, int high,String str) {
-        boolean check = false;
-        while (low < high) {
-            if(str.charAt(low)==str.charAt(high)) {
-                low++;
-                high--;
-                check = true;
-            }else{
-              return  false;
+    private fun isPalin(inputString: String): Boolean {
+        val set = HashSet<Char>()
+        inputString.forEach {
+            if(set.contains(it)){
+                set.remove(it)
+            } else{
+                set.add(it)
             }
         }
-        return check;
+        return set.size<=1 // is palindrome if there is 1 element or less left
+    }
+
+    private fun possiblePalinByRemovingOneChar(str: String): Int {
+        var low = 0
+        var high = str.length - 1
+        while (low < high) {
+            if (str[low] == str[high]) {
+                low++
+                high--
+            } else {
+                // check if removing the left char forms a palindrome
+                if (isPalindrome(low + 1, high, str)) return low
+
+                // check if removing the right char forms a palindrome
+                if (isPalindrome(low, high - 1, str)) return high
+            }
+        }
+        return -1
+    }
+
+    private fun isPalindrome(mLow: Int, mHigh: Int, str: String): Boolean {
+        var low = mLow
+        var high = mHigh
+        var check = false
+        while (low < high) {
+            check = if (str[low] == str[high]) {
+                low++
+                high--
+                true
+            } else {
+                return false
+            }
+        }
+        return check
     }
 }
