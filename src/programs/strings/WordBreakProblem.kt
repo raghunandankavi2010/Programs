1c6f91082b37d93d0e6d1351467eca300582759d
Raghunandan Kavi <raghunandan2005@gmail.com>
2021-02-22 14:30:01 +0530

    Minor Changes.

--- src/programs/strings/WordBreakProblem.kt
+++ src/programs/strings/WordBreakProblem.kt
@@ -2,45 +2,45 @@ package programs.strings
 
 /**
  * Input:
-s = "catsanddog"
-wordDict = ["cat", "cats", "and", "sand", "dog"]
-Output:
-[
-"cats and dog",
-"cat sand dog"
+ * s = "catsanddog"
+ * wordDict = ["cat", "cats", "and", "sand", "dog"]
+ * Output:
+ * [
+ * "cats and dog",
+ * "cat sand dog"
 ]
  */
 
 val map = HashMap<String,List<String>>()
+
 fun main() {
     val inputString = "catsanddog"
     val wordDict = listOf<String>("cat", "cats", "and", "sand", "dog")
-
     wordBreak(inputString, wordDict)?.forEach {
         println(it)
     }
 }
 
-fun wordBreak(inputString: String, wordDict: List<String>): List<String>? {
-    if(map.containsKey(inputString))
-        return map[inputString]
+fun wordBreak(s: String, wordDict: List<String>): List<String>? {
+    if(map.containsKey(s))
+        return map[s]
 
     val result = ArrayList<String>()
 
-    if(wordDict.contains(inputString)){
-        result.add(inputString)
+    if(wordDict.contains(s)){
+        result.add(s)
     }
 
-    for(i in 1 until inputString.length) {
-        val left = inputString.substring(0,i)
+    for(i in 1 until s.length) {
+        val left = s.substring(0,i)
         if(wordDict.contains(left)) {
-            val list = wordBreak(inputString.substring(i),wordDict)
+            val list = wordBreak(s.substring(i),wordDict)
             list?.forEach {
                 result.add("$left $it")
             }
         }
     }
-    map[inputString] = result
+    map[s] = result
     return result
 
 }
