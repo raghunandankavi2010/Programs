6a86e91571d27df84a65be2064d531c95308915f
Raghunandan Kavi <raghunandan2005@gmail.com>
2021-07-16 16:18:29 +0530

    Sort arrays of 0 and 1

---
+++ src/programs/arrays/SortZerosAndOnes.kt
@@ -0,0 +1,22 @@
+package programs.arrays
+
+/**
+ * Sort array of zeros and 1
+ */
+
+fun main() {
+    val array = arrayOf(1, 0, 1, 1, 0, 1, 0, 0)
+
+    var left = 0;
+    var right = array.size -1
+    while(left > right) {
+        if(array[left] == 1) {
+            val temp = array[left]
+            array[left] = array[right]
+            array[right] = temp
+            right --
+        } else {
+            left ++
+        }
+    }
+}
