d7caa09c2a4f5dd63c58a3d10f8628ca9b781977
Raghunandan Kavi <raghunandan2005@gmail.com>
2021-07-16 16:24:24 +0530

    Sort arrays of 0 and 1

--- src/programs/arrays/SortZerosAndOnes.kt
+++ src/programs/arrays/SortZerosAndOnes.kt
@@ -1,12 +1,21 @@
 package programs.arrays
 
+import java.util.*
+
 /**
  * Sort array of zeros and 1
  */
 
 fun main() {
     val array = arrayOf(1, 0, 1, 1, 0, 1, 0, 0)
+    bubbleSort(array)
+    sortArray(array)
 
+
+
+}
+
+fun sortArray(array: Array<Int>) {
     var left = 0
     var right = array.size -1
     while(left > right) {
@@ -19,4 +28,19 @@ fun main() {
             left ++
         }
     }
+    println(array.contentToString())
+}
+
+fun bubbleSort(array: Array<Int>) {
+    val n = array.size - 1
+    for(i in 0.. n) {
+        for( j in 0.. n-(i+1)) {
+            if(array[j] >= array[j+1]) {
+                val temp = array[j]
+                array[j] = array[j+1]
+                array[j+1] = temp
+            }
+        }
+    }
+    println(array.contentToString())
 }
