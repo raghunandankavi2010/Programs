689f43af7f9ebef99e63b8fb557ea41083b66401
Raghunandan Kavi <raghunandan2005@gmail.com>
2021-12-09 16:51:52 +0530

    Reverse words in sentences.

--- src/programs/strings/ReverseWordsInString.java
+++ src/programs/strings/ReverseWordsInString.java
@@ -1,5 +1,7 @@
 package programs.strings;
 
+import java.util.Arrays;
+
 /**
  * https://leetcode.com/problems/reverse-words-in-a-string/submissions/
  */
@@ -12,6 +14,7 @@ public class ReverseWordsInString {
         // philips test.
         String s2 = "I am working at Manyata";
         reverseWordsInString.reverseWords(s2);
+
 
     }
 
@@ -38,6 +41,40 @@ public class ReverseWordsInString {
 
     }
 
+    private String reverseWordsInString(String input) {
+        char[] s = input.toCharArray();
+        int start = 0;
+        for (int end = 0; end < s.length; end++) {
+            if(s[end] == ' ') {
+                reverse(s,start, end);
+                start= end  + 1;
+            }
+        }
+
+        reverse(s, start, s.length - 1);
+
+        reverse(s, 0, s.length - 1);
+
+        return Arrays.toString(s);
+
+    }
+
+    private void reverse(char[] str, int start, int end) {
+        // Temporary variable
+        // to store character
+        char temp;
+
+        while (start <= end) {
+            // Swapping the first
+            // and last character
+            temp = str[start];
+            str[start] = str[end];
+            str[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
     private void reverse(String s) {
         s = s.replaceAll("\\s+", " ").trim();
         String[] strgs = s.split("\\s");
@@ -54,4 +91,5 @@ public class ReverseWordsInString {
         }
         System.out.println("Reversed String : " + sb.toString());
     }
+
 }
