50967653a8cc86d138b2843a5dc3217952e2baf3
Raghunandan Kavi <raghunandan2005@gmail.com>
2022-01-29 12:08:04 +0530

    Kadane's algorithm with start and end index of sub-array.

--- src/programs/arrays/KadaneAlgo.kt
+++ src/programs/arrays/KadaneAlgo.kt
@@ -1,21 +1,29 @@
 package programs.arrays
 
 fun main() {
-    val array = arrayOf(-2, 1, -3, 4, -1, 2, 1, -5, 4)
+    val array = arrayOf(-2, -3, 4, -1, -2, 1, 5, -3)
 
     var currSum = 0
     var maxSum = 0
-    array.forEach {
-        currSum += it
+    var start = 0
+    var end = 0
+    var sIndex = 0
+    array.forEachIndexed { index, element ->
+        currSum += element
 
         if(currSum > maxSum) {
             maxSum = currSum
+            start = sIndex
+            end = index
         }
 
         if(currSum < 0) {
             currSum = 0
+            sIndex = index +1
         }
     }
 
     println(maxSum)
+    println("Start Index: $start")
+    println("End Index: $end")
 }
