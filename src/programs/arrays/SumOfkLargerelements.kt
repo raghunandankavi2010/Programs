b0f51a48d79e4d0d364fb2f658ced0863a0bf77d
raghunandan <raghunandan@mesh.ai>
2022-03-16 18:20:35 +0530

    K largest elements only even and sum of those elements

---
+++ src/programs/arrays/SumOfkLargerelements.kt
@@ -0,0 +1,39 @@
+package programs.arrays
+
+import java.util.*
+
+
+fun main() {
+
+    //12 6 4
+    val array = intArrayOf(12, 3, 4, 1, 6, 9,20)
+    val k = 3
+    val minHeap = PriorityQueue<Int>()
+    var index = 0
+    var count = 0
+    while (count < k && index < array.size) {
+        if (array[index] % 2 == 0) {
+            minHeap.add(array[index])
+            count++
+        }
+        index++
+    }
+
+    for (i in k until array.size) {
+        if (minHeap.peek() > array[i]) continue else {
+            if (array[i] % 2 == 0 && !minHeap.contains(array[i])) {
+                minHeap.poll()
+                minHeap.add(array[i])
+            }
+        }
+    }
+    var max = 0
+
+    val iterator: Iterator<Int> = minHeap.iterator()
+
+    while (iterator.hasNext()) {
+        max += iterator.next()
+    }
+
+    println(max)
+}
