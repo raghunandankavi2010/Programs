70fffee5290c61412b940ea4db2f1fa297697f48
Raghunandan <raghunandan2005@gmail.com>
2022-04-04 16:00:31 +0530

    Decode String using two stacks.

--- src/programs/strings/DecodeString.kt
+++ src/programs/strings/DecodeString.kt
@@ -1,5 +1,7 @@
 package programs.strings
 
+import java.util.*
+
 /**
  * Given the mapping a = 1, b = 2, ... z = 26, and an encoded message, count the number of ways it can be decoded.
  * For example, the message '111' would give 3, since it could be decoded as 'aaa', 'ka', and 'ak'.
@@ -9,6 +11,41 @@ package programs.strings
 fun main() {
     val digits = charArrayOf('1', '1', '1')
     println(countDecodingDP(digits,3))
+    val s = "3[a2[c]]"
+    val decodedString = decode(s)
+    println(decodedString)
+}
+
+fun decode(s: String): String {
+    val counts = Stack<Int>()
+    val chars = Stack<String>()
+    var res = ""
+    var index = 0
+    while(index < s.length)
+        if(s[index].isDigit()) {
+            var num = 0
+            while(s[index].isDigit()) {
+                num = num * 10 + (s[index] - '0')
+                index++
+            }
+            counts.add(num)
+        } else if(s[index] == '[') {
+            chars.add(res)
+            res = ""
+            index++
+        } else if(s[index] == ']'){
+            val sb = StringBuilder(chars.pop())
+            val count = counts.pop()
+            for(i in 0 until count) {
+                sb.append(res)
+            }
+            res = sb.toString()
+            index++
+        } else {
+            res += s[index]
+            index++
+        }
+return res
 }
 
 fun countDecodingDP(digits: CharArray, n: Int): Int {
