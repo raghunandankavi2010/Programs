276935b700b3527c8a9f0226dbbe618e4ddf3626
Raghunandan <raghunandan2005@gmail.com>
2022-05-19 11:10:52 +0530

    Longest Common Subsequence with tracing

--- src/programs/strings/LongestCommonSubsequence.kt
+++ src/programs/strings/LongestCommonSubsequence.kt
@@ -5,11 +5,10 @@ package programs.strings
  */
 
 fun main() {
-    val str1 = "ABCDGHLQR"
-    val str2 = "AEDPHR"
 
+    val str1 = "AGGTAB"
+    val str2  = "GXTXAYB"
     val result: Int = lcsDynamic(str1.toCharArray(), str2.toCharArray())
-    print(result)
 }
 
 fun lcsDynamic(str1: CharArray, str2: CharArray): Int {
@@ -27,5 +26,31 @@ fun lcsDynamic(str1: CharArray, str2: CharArray): Int {
             }
         }
     }
+
+    // Tracing part
+    var index = temp[str1.size][str2.size]
+    val tempIndex = index
+    var m = str1.size
+    var n = str2.size
+    val lcsArray = CharArray(tempIndex + 1)
+    lcsArray[tempIndex] = 0.toChar()
+
+    while(m > 0 && n > 0) {
+        if(str1[m -1] == str2[n -1]) { // if char starting from last are same
+            lcsArray[index-1] = str1[m-1] // copy that to temp array and decrement indexes
+            m--
+            n--
+            index --
+        } else if(temp[m-1] [n] > temp[m] [n-1]) { // if value of char of s1 is greater than s2 decrement left index
+            m--
+        } else {
+            n--
+        }
+    }
+    // print the temp array
+    for (k in 0 until tempIndex) print(lcsArray[k])
+
     return max
 }
+
+
