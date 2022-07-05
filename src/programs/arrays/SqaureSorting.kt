4681a154f2e76783d5028caf249cd7e04e16c6a3
Raghunandan <raghunandan2005@gmail.com>
2022-07-05 15:58:19 +0530

    Minor Chnges.

--- src/programs/arrays/SqaureSorting.kt
+++ src/programs/arrays/SqaureSorting.kt
@@ -14,54 +14,50 @@ import kotlin.math.abs
 
 fun main() {
     println("Merge two sorted Array")
-    mergeArray()
+    val nums1 = arrayOf(1,2,3,0,0,0)
+    val m = 3
+    val nums2 = arrayOf(2,5,6)
+    val n = 3
+    mergeArray(nums1,m,nums2,n)
     println("\nSquare and sort a sorted array")
     squareSort()
 }
 
-fun mergeArray() {
-    val inputArray1 = arrayOf(1)
-    val inputArray2 = arrayOf(1,1,2)
+fun mergeArray(nums1: Array<Int>, m: Int, nums2: Array<Int>, n: Int) {
+    val inputArray1 = nums1
+    val inputArray2 = nums2
 
-    var p1 = inputArray1.size - 1
-    var p2 = inputArray2.size - 1
-    val outputArray = IntArray(inputArray1.size + inputArray2.size) { 0 }
+    var p1 = m - 1
+    var p2 = n - 1
+    val outputArray = IntArray(m + n) { 0 }
 
-    var n = outputArray.size - 1
+    var index = m + n- 1
 
 
     while (p1 >= 0 && p2 >= 0) {
-        if (inputArray1[p1] == 0) {
-            p1--
-        } else if (inputArray2[p2] == 0) {
-            p2--
-        }
         if (inputArray1[p1] >= inputArray2[p2]) {
-            outputArray[n] = inputArray1[p1]
-            p1--
+            outputArray[index] = inputArray1[p1]
+           --p1
         } else if(inputArray1[p1] < inputArray2[p2]){
-            outputArray[n] = inputArray2[p2]
-            p2--
+            outputArray[index] = inputArray2[p2]
+            --p2
         }
-        n--
+        --index
     }
 
-
     while (n >= 0 && p1 >= 0) {
-        outputArray[n] = inputArray1[p1]
+        outputArray[index] = inputArray1[p1]
         p1--
-        n--
+        index--
     }
 
     while (n >= 0 && p2 >= 0) {
-        outputArray[n] = inputArray2[p2]
+        outputArray[index] = inputArray2[p2]
         p2--
-        n--
+        index--
     }
 
-    val resultArray = outputArray.filter { it -> it != 0 }.toIntArray()
-
-    resultArray.forEach { num ->
+    outputArray.forEach { num ->
         print("$num ")
     }
 }
