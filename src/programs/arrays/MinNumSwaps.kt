0d0e1a8327f871d21cfeee1ea0b50c0a1964474d
Raghunandan <raghunandan2005@gmail.com>
2022-07-29 07:55:46 +0530

    Add explanation

--- src/programs/arrays/MinNumSwaps.kt
+++ src/programs/arrays/MinNumSwaps.kt
@@ -6,10 +6,12 @@ package programs.arrays
 fun main() {
     val arr = arrayOf(1,0,1,0,1,0,1,1,1)
 
+    // calcualte total number of ones. that will be our window
     val totalOnes = arr.count { it == 1}
     var currZeros = 0
     var ans: Int
 
+    // calculate the number of zeros in the first windpow
     for(i in 0 until totalOnes) {
         if(arr[i] == 0) {
             currZeros += 1
@@ -17,7 +19,9 @@ fun main() {
     }
     ans = currZeros
 
-
+    // calculate number of zeros in from 2nd window to last
+    // if the window size exceeds remove the first item by doing minus 1
+    // update the ans
     for(i in totalOnes until arr.size) {
         if(arr[i] == 0) {
             currZeros += 1
