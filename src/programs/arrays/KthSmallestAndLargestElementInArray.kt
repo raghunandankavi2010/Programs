a63c9359e792c33c19cb65a8f36f07a27c7b0544
Raghunandan <raghunandan2005@gmail.com>
2022-11-26 10:49:37 +0530

    Convert java code to kotlin

--- src/programs/arrays/KthSmallestAndLargestElementInArray.kt
+++ src/programs/arrays/KthSmallestAndLargestElementInArray.kt
@@ -1,23 +1,22 @@
-package programs.arrays;
+package programs.arrays
 
-import java.util.Comparator;
-import java.util.PriorityQueue;
+import java.util.*
 
-public class KthSmallestAndLargestElementInArray {
-
-    public static void main(String[] args) {
-        int[] arr= {2,3,15,1,0,23,21};
-        int k=2;
-        int kthSmallestElement = findKthSmallestElement(arr,k);
-        int kthLargestElement = findKthLargestElement(arr,k);
-        System.out.printf("Smallest %d element is %d%n",k,kthSmallestElement);
-        System.out.printf("Largest %d element is %d%n",k,kthLargestElement);
+object KthSmallestAndLargestElementInArray {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val arr = intArrayOf(2, 3, 15, 1, 0, 23, 21)
+        val k = 2
+        val kthSmallestElement = findKthSmallestElement(arr, k)
+        val kthLargestElement = findKthLargestElement(arr, k)
+        System.out.printf("Smallest %d element is %d%n", k, kthSmallestElement)
+        System.out.printf("Largest %d element is %d%n", k, kthLargestElement)
     }
 
-    private static int findKthSmallestElement(int[] arr, int k) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
-        for(int i=0;i<k;i++){
-            pq.add(arr[i]); // 3 2
+    private fun findKthSmallestElement(arr: IntArray, k: Int): Int {
+        val pq = PriorityQueue(Comparator.reverseOrder<Int>())
+        for (i in 0 until k) {
+            pq.add(arr[i]) // 3 2
         }
         // from 1 to 21
         // arr[i] now is 1
@@ -25,19 +24,19 @@ public class KthSmallestAndLargestElementInArray {
         // 3 2
         // remove 3 from queue
         // 2 1 0
-        for(int i=k;i<arr.length;i++){
-            if(arr[i]< pq.peek()){
-                pq.poll(); // remove from queue
-                pq.add(arr[i]);
+        for (i in k until arr.size) {
+            if (arr[i] < pq.peek()) {
+                pq.poll() // remove from queue
+                pq.add(arr[i])
             }
         }
-        return pq.peek(); // return 2
+        return pq.peek() // return 2
     }
 
-    private static int findKthLargestElement(int[] arr, int k) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        for(int i=0;i<k;i++){
-            pq.add(arr[i]); //2 3
+    private fun findKthLargestElement(arr: IntArray, k: Int): Int {
+        val pq = PriorityQueue<Int>()
+        for (i in 0 until k) {
+            pq.add(arr[i]) //2 3
         }
         // from 1 to 21
         // arr[i] now is 1
@@ -50,12 +49,12 @@ public class KthSmallestAndLargestElementInArray {
         // remove 3 and add 21
         // 15 23 21
         // return 15
-        for(int i=k;i<arr.length;i++){
-            if(arr[i]>pq.peek()){
-                pq.poll();
-                pq.add(arr[i]);
+        for (i in k until arr.size) {
+            if (arr[i] > pq.peek()) {
+                pq.poll()
+                pq.add(arr[i])
             }
         }
-        return pq.peek();
+        return pq.peek()
     }
 }
