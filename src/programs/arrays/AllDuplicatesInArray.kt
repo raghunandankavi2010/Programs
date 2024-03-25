c728813faedc8a8d3ce1585606d39ae6d7f876b1
Raghunandan K <raghunandankavi2010@gmail.com>
2024-03-25 13:42:36 +0530

    Leet code 442

---
+++ src/programs/arrays/AllDuplicatesInArray.kt
@@ -0,0 +1,52 @@
+package programs.arrays
+
+import kotlin.math.abs
+
+/**
+ * Leetcode 442. Find All Duplicates in an Array
+ * Given an integer array nums of length n where all the integers of nums are in the range [1, n] and each integer appears once or twice, return an array of all the integers that appears twice.
+ *
+ * You must write an algorithm that runs in O(n) time and uses only constant extra space.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: nums = [4,3,2,7,8,2,3,1]
+ * Output: [2,3]
+ * Example 2:
+ *
+ * Input: nums = [1,1,2]
+ * Output: [1]
+ * Example 3:
+ *
+ * Input: nums = [1]
+ * Output: []
+ *
+ *
+ * Constraints:
+ *
+ * n == nums.length
+ * 1 <= n <= 105
+ * 1 <= nums[i] <= n
+ * Each element in nums appears once or twice.
+ */
+
+fun main() {
+   val num = intArrayOf(4,3,2,7,8,2,3,1)
+    val list = findDuplicatesNums(num)
+    println(list)
+
+}
+fun findDuplicatesNums(nums: IntArray): List<Int> {
+    val result = mutableListOf<Int>()
+    val n = nums.size
+    for (i in 0 until n) {
+        val num = abs(nums[i])
+        val idx = num - 1
+        if (nums[idx] < 0)
+            result.add(num)
+        nums[idx] *= -1
+    }
+    return result
+}
